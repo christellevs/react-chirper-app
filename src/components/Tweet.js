da082015e40dd13ff9dc09a5bd5fecdd5dd56d92
Christelle Van Sebroeck <christelle.marie@gmail.com>
2022-08-26 16:40:59 +0100

    feat: tweet ui part 1

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,16 +1,53 @@
 import { connect } from "react-redux";
-import { formatTweet } from "../utils/helpers";
+import { formatTweet, formatDate } from "../utils/helpers";
+
+import {
+  TiArrowBackOutline,
+  TiHeartOutline,
+  TiHeartFullOutline,
+} from "react-icons";
 
 const Tweet = (props) => {
-  console.log(props);
-  return <div className="tweet"></div>;
+  if (props.tweet === null) {
+    return <p>This Tweet doesn't exist</p>;
+  }
+
+  const toParent = (e, id) => {
+    e.preventDefault();
+
+    // TODO - redirect to parent tweet
+  };
+
+  const { name, avatar, timestamp, text, hasLiked, likes, replies, parent } =
+    props.tweet;
+  return (
+    <div className="tweet">
+      <img src={avatar} alt={`Avatar of ${name}`} className="avatar" />
+      <div className="tweet-info">
+        <span>{name}</span>
+        <div>{formatDate(timestamp)}</div>
+        {parent && (
+          <button
+            className="replying-to"
+            onClick={(e) => toParent(e, parent.id)}
+          >
+            Replying to @{parent.author}
+          </button>
+        )}
+        <p>{text}</p>
+      </div>
+    </div>
+  );
 };
 
 const mapStateToProps = ({ authedUser, users, tweets }, { id }) => {
   const tweet = tweets[id];
+  const parentTweet = tweet ? tweets[tweet.replyingTo] : null;
   return {
     authedUser,
-    tweet: formatTweet(tweet, users[tweet.author], authedUser),
+    tweet: tweet
+      ? formatTweet(tweet, users[tweet.author], authedUser, parentTweet)
+      : null,
   };
 };
 
