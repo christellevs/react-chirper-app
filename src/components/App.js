f05f9e6483bea7cb6a72ca4106e0ab44bb521ff9
Christelle Van Sebroeck <christelle.marie@gmail.com>
2022-08-26 20:42:57 +0100

    feat: add react router part 1

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
-import { useEffect } from "react";
+import { useEffect, Fragment } from "react";
 import { connect } from "react-redux";
 import LoadingBar from "react-redux-loading-bar";
+import { Routes, Route } from "react-router-dom";
 import { handleInitialData } from "../actions/shared";
 import Dashboard from "./Dashboard.js";
 import NewTweet from "./NewTweet.js";
 import TweetPage from "./TweetPage.js";
+import Nav from "./Nav.js";
 
 const App = (props) => {
   useEffect(() => {
@@ -13,10 +15,19 @@ const App = (props) => {
 
   return (
     <div>
-      <LoadingBar />
-      {props.loading === true ? null : (
-        <TweetPage match={{ params: { id: "8xf0y6ziyjabvozdd253nd" } }} />
-      )}
+      <Fragment>
+        <LoadingBar />
+        <div className="container">
+          <Nav />
+          {props.loading === true ? null : (
+            <Routes>
+              <Route path="/" exact element={<Dashboard />} />
+              <Route path="/tweet/:id" exact element={<TweetPage />} />
+              <Route path="/new" exact element={<NewTweet />} />
+            </Routes>
+          )}
+        </div>
+      </Fragment>
     </div>
   );
 };
